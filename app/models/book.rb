b75197c9b2ba0d791160fc32ae016b986b2e39b1
Masaya Kawamura <masayakawamura@MasayanoMacBook-Pro.local>
2022-02-11 23:59:53 +0900

    First Commit

---
+++ app/models/book.rb
@@ -0,0 +1,5 @@
+class Book < ApplicationRecord
+    scope :costly, -> { where("price > ?", 3000) }
+    scope :written_about, ->(them) { where("name like ?", "%#{them}%") }
+    scope :find_price, ->(price) { find_by(price: price) }
+end
