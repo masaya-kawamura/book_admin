d9a1a3ad7af903ee897e66607f761a6b714edc0c
Masaya Kawamura <masayakawamura@MasayanoMacBook-Pro.local>
2022-02-12 00:44:18 +0900

    [add]著者、技術評論社に対応するモデルを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+    belongs_to :publisher
+
     scope :costly, -> { where("price > ?", 3000) }
     scope :written_about, ->(them) { where("name like ?", "%#{them}%") }
     scope :find_price, ->(price) { find_by(price: price) }
